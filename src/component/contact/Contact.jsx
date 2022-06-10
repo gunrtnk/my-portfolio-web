b38481bc6bd2483f93f6d0675dfec79a00033cf6
gunrtnk <rattanakorn.crk@gmail.com>
2022-06-11 01:19:36 +0700

    update style

--- src/component/contact/Contact.jsx
+++ src/component/contact/Contact.jsx
@@ -2,26 +2,60 @@ import React, { useRef, useState } from "react";
 import "./Contact.css";
 import Img1 from "../../img/6.svg";
 
-import emailjs from '@emailjs/browser';
+import emailjs from "@emailjs/browser";
 const Contact = () => {
+  const form = useRef();
+  const [done, setDone] = useState(false);
+  let [resetForm, resetInputField ] = useState(false);
+
+  const [inputValue, setUserValue] = useState("");
+  const [inputValue2, setEmailValue] = useState("");
+  const [inputValue3, setMsgValue] = useState("");
+
+  const handleUserInput = (e) => {
+    setUserValue(e.target.value);
+  };
+  const handleEmailInput = (e) => {
+    setEmailValue(e.target.value);
+  };
+  const handleMsgInput = (e) => {
+    setMsgValue(e.target.value);
+  };
+
+  resetInputField = () => {
+    setUserValue("");
+    setEmailValue("");
+    setMsgValue("");
+  };
+
   
 
-    const form = useRef();
-    const [done,setDone] = useState(false);
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-    const sendEmail = (e) => {
-      e.preventDefault();
-  
-      emailjs.sendForm('service_iiwy6qc', 'template_qaumgpe', form.current, 'VJMdp457kvLcqTiEd')
-        .then((result) => {
-            console.log(result.text);
-            setDone(true);
-        }, (error) => {
-            console.log(error.text);
-        });
-    };
+    emailjs
+      .sendForm(
+        "service_iiwy6qc",
+        "template_qaumgpe",
+        form.current,
+        "VJMdp457kvLcqTiEd"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setDone(true);
 
-return (
+          resetInputField(true)
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+
+
+  return (
     <div>
       <div className="c-container" id="Contact">
         <div className="map">
@@ -30,34 +64,41 @@ return (
         <div className="c-right">
           <div className="c-detail">
             <h2>CONTACT ME</h2>
-            
+
             <div className="cf-con">
-              <form ref={form} onSubmit={sendEmail} className="c-form">
-                
+              <form ref={form} onSubmit={resetForm} className="c-form">
                 <input
                   type="text"
                   name="user_name"
                   className="user"
                   placeholder="   Your Name"
+                  value={inputValue}
+                  onChange={handleUserInput}
                 ></input>
                 <input
                   type="email"
                   name="user_email"
                   className="email"
                   placeholder="   Your Email"
+                  value={inputValue2}
+                  onChange={handleEmailInput}
                 ></input>
                 <textarea
                   name="message"
                   className="usermsg"
                   placeholder="  Message"
+                  value={inputValue3}
+                  onChange={handleMsgInput}
                 ></textarea>
                 <input
                   type="submit"
                   value="Send"
                   name="user_name"
                   className="button ct"
+                  onClick={sendEmail}
                 ></input>
-                <span>{done && "Thank you for contact me (:" }</span>
+                <span>{done && "Thank you for contact me (:"}</span>
+                
               </form>
             </div>
           </div>
